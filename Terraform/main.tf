d26c59c14f38d85c35713cd2f4d6603ec3afbb8f
CyganK <konrad.cygan@britenet.com.pl>
2024-02-09 18:30:55 +0100

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Konto Magazynowe
 resource "azurerm_storage_account" "sa" {
-  name                     = "ProjektZespolowySA"
+  name                     = "projektzespolowysa"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -14,7 +14,7 @@ resource "azurerm_storage_account" "sa" {
 
 # Kontener Blob
 resource "azurerm_storage_container" "blob_container" {
-  name                  = "PZblobContainer"
+  name                  = "pzblobcontainer"
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
